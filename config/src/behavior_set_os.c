fd46fe2faf9a92d1e8f08be7cecfab04259d1697
oleggud512 <oleggud52@gmail.com>
2026-07-24 14:04:45 +0200

    Fixed os switch

--- config/src/behavior_set_os.c
+++ config/src/behavior_set_os.c
@@ -30,11 +30,11 @@ static const struct behavior_driver_api behavior_set_os_driver_api = {
     .binding_released = on_keymap_binding_released,
 };
 
-#define SET_OS_INST(n)                                                      \
-    static struct behavior_set_os_config config_##n = {};                   \
-    static struct behavior_set_os_data data_##n = {};                       \
-    DEVICE_DT_INST_DEFINE(n, NULL, NULL, &data_##n, &config_##n,            \
-                          POST_KERNEL, CONFIG_KERNEL_INIT_PRIORITY_DEFAULT, \
-                          &behavior_set_os_driver_api);
+#define SET_OS_INST(n)                                                        \
+    static struct behavior_set_os_config config_##n = {};                     \
+    static struct behavior_set_os_data data_##n = {};                         \
+    BEHAVIOR_DT_INST_DEFINE(n, NULL, NULL, &data_##n, &config_##n,            \
+                            POST_KERNEL, CONFIG_KERNEL_INIT_PRIORITY_DEFAULT, \
+                            &behavior_set_os_driver_api);
 
 DT_INST_FOREACH_STATUS_OKAY(SET_OS_INST)
